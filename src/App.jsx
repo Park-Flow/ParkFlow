4f4efbf99a967ad6d1ef8194896bad311f346972
João Victor <jvictor.franci@gmail.com>
2023-09-20 14:22:14 -0300

    :tada: Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "./contexts/globalStyles.jsx";
+function App() {
+  
+  return (
+    <>
+      <GlobalStyles />
+      <h1>AAAAAAAAAAAAAAAAAAAAA</h1>
+    </>
+  );
+  
+}
+export default App;
